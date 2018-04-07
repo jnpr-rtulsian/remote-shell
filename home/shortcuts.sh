0f959b304ef8a4790ec1d91f44dea7e33c21c4a7
jnpr-rtulsian <rtulsian@juniper.net>
2018-04-06 23:11:25 -0700

    Added build aliases

--- home/shortcuts.sh
+++ home/shortcuts.sh
@@ -4,3 +4,9 @@ export LD_LIBRARY_PATH=$CONTRAIL/build/lib
 alias mymux='tmux attach -t rishabh || tmux new -s rishabh'
 alias ff='find . -name '
 alias cdc='cd $CONTRAIL'
+
+alias fabric.scons='scons controller/src/config/fabric-ansible'
+alias fabric.venv='source $CONTRAIL/build/debug/config/fabric-ansible/.venv/bin/activate'
+
+alias api.scons='scons controller/src/config/api-server'
+alias api.venv='source $CONTRAIL/build/debug/config/api-server/.tox/cover/bin/activate'
